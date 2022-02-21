dde03eba14be13b344f1f43918dd65296c256f35
unknown <kartikgarg938@gmail.com>
2022-02-22 00:51:50 +0530

    adding handlebars and styling

--- src/app.js
+++ src/app.js
@@ -1,15 +1,47 @@
 const path = require("path")
 const express = require("express")
 const app = express()
+const hbs = require('hbs')
 
 // console.log(__dirname)
 // console.log(path.join(__dirname, "../public"))
 
 const publicDirectoryPath = path.join(__dirname, '../public')
-
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 //.use is to customise server
-
 app.use(express.static(publicDirectoryPath))
+
+//.set allows to set a value for given express 'property'
+//setup handlebars and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.get('', (req,res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help')
+})
+
+app.get('/index', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req,res) => {
+    res.render('about')
+})
+
+app.get('*', (req, res) =>{
+    res.render('404', {
+        errorMessage: '404 error'
+    })
+})
 
 const port = 3000
 
